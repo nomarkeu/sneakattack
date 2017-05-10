624e3a4774ee752fbe1a24fee813d885756d8340
Sooraj Raj <none@none.com>
2017-05-10 00:39:43 -0500

    main cleared up

--- MapGenerator/mapgenerator.cpp
+++ MapGenerator/mapgenerator.cpp
@@ -6,62 +6,8 @@ using std::vector;
 int main()
 {
 
-	
 	MapGenerator app;
 	app.start();
 
-
-//	bool undotrigger = false; //1299 768
-//	bool undo = false;
-//	vector<sf::RectangleShape> rectangles;
-//	sf::Vector2f rectanglesize;
-//	sf::RenderWindow window(sf::VideoMode(800, 600), "Map Generator");//,sf::Style::Fullscreen);
-//	sf::Rect<float> rect(static_cast<sf::Vector2<float> >(window.getPosition()), static_cast<sf::Vector2<float> >(window.getSize()));
-//
-//	sf::VertexArray MapLines(sf::Lines);
-//	
-	//sf::Texture texture;
-	//if (!texture.loadFromFile("background.jpg"))
-	//	throw std::runtime_error("Could not load file player.png");
-
-
-	//sf::Sprite background;
-	//background.setTexture(texture);
-	//background.setTextureRect(sf::IntRect(0, 0, 1920, 1200));
-	//background.setScale(sf::Vector2f(.64, .64));
-//
-//	bool leftClick = false;
-//	bool rightClick = false;
-//	bool draw = false;
-//	bool secondPointSet = false;
-//	bool start = true;
-//	bool canStartANewLine = true;
-//
-//	int i = 0;
-//	sf::Vector2f& startpoint;
-//	sf::Vector2f endpoint;
-//
-//	//sf::Event event;
-//	//start drawing
-//
-//	//draw is always false in this loop level
-//	while (window.isOpen()) {
-//		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-//			break;
-//		
-//
-//		drawline(startpoint, MapLines, i, window);
-//
-//		window.clear(sf::Color::White);
-//
-//		window.draw(background);
-//		window.draw(MapLines);
-//		
-//		window.display();
-//} 
-//
-//
-//	window.close();
-//	
 	return 0;
 }
